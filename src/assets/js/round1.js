9cc40952ee957b2392880e14b1acba7758516b3f
biubiubiupiu21 <94020092+biubiubiupiu21@users.noreply.github.com>
2022-11-21 16:51:16 -0500

    12

---
+++ src/assets/js/round1.js
@@ -0,0 +1,29 @@
+function win(){
+    console.log("xxx")
+}
+
+function load_data(){
+    student_data = localStorage.getItem("data");
+    student_data = JSON.parse(student_data);
+    console.log(student_data);
+  
+    $('.studentName').each(function(i, obj) {
+      $( this ).text(student_data[i].name)
+    });
+  
+    $('.songName').each(function(i, obj) {
+      $( this ).text(student_data[i].song)
+    });
+  
+    $('.score').each(function(i, obj) {
+      $( this ).text(student_data[i].total)
+    });
+  
+    $('.col-12').each(function(i, obj) {
+      $( this ).attr('id',i);
+    });
+    
+    student_data = JSON.stringify(student_data)
+    localStorage.setItem("displayData", student_data);
+
+}
