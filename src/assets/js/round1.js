174a09dde1b0e007107bc371558bdeb489fca5e8
Jason Pei <peijiannuo@gmail.com>
2022-12-01 21:04:02 -0500

    changed round 1

--- src/assets/js/round1.js
+++ src/assets/js/round1.js
@@ -1,34 +1,33 @@
-function win(card){
-  $("."+card).addClass("win")
-  $("."+card+" .btn").addClass("winBtn")
-  $("."+card+" .card-img-top").addClass("winImg")
+function win(card) {
+  $("." + card).addClass("win");
+  $("." + card + " .btn").addClass("win-btn");
+  $("." + card + " .card-img-top").addClass("win-img");
 }
-function lost(card){
-  $("."+card).addClass("lost")
+function lose(card) {
+  $("." + card).addClass("lose");
 }
 
-function load_data(){
-    student_data = localStorage.getItem("data");
-    student_data = JSON.parse(student_data);
-    console.log(student_data);
-  
-    $('.studentName').each(function(i, obj) {
-      $( this ).text(student_data[i].name)
-    });
-  
-    $('.songName').each(function(i, obj) {
-      $( this ).text(student_data[i].song)
-    });
-  
-    $('.score').each(function(i, obj) {
-      $( this ).text(student_data[i].total)
-    });
+function load_data() {
+  student_data = localStorage.getItem("data");
+  student_data = JSON.parse(student_data);
+  console.log(student_data);
 
-    $('.card-img-top').each(function(i, obj) {
-      $( this ).attr('src',student_data[i].img);
-    });
-    
-    student_data = JSON.stringify(student_data)
-    localStorage.setItem("displayData", student_data);
+  $(".studentName").each(function (i, obj) {
+    $(this).text(student_data[i].name);
+  });
 
+  $(".songName").each(function (i, obj) {
+    $(this).text(student_data[i].song);
+  });
+
+  $(".score").each(function (i, obj) {
+    $(this).text(student_data[i].total);
+  });
+
+  $(".card-img-top").each(function (i, obj) {
+    $(this).attr("src", student_data[i].img);
+  });
+
+  student_data = JSON.stringify(student_data);
+  localStorage.setItem("displayData", student_data);
 }
